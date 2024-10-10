56481f8896232adc1c360ca421b13a6e28d2a7ba
Sadman Sakib Abesh <sadmansakibabesh@gmail.com>
2024-10-11 01:39:29 +0600

    Create exercise_3.cpp

---
+++ exercise/c++/exercise_3.cpp
@@ -0,0 +1,18 @@
+//find out the area of a circle
+
+#include <cmath>
+
+using namespace std;
+//solve
+int main(){
+    //cmath exercise
+    double r;
+    double pi=3.1416;
+    double area;
+cout<<"provide the radius"<<endl;
+
+cin >> r;
+area =pi*pow(r,2);
+cout<<"the area of the circle is "<<area;
+   return 0;
+}
