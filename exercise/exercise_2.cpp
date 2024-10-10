b8e7389c4cadf6b2eaf5aae5a88bfa37c93f8794
Sadman Sakib Abesh <sadmansakibabesh@gmail.com>
2024-10-11 00:19:31 +0600

    Create exercise_2.cpp

---
+++ exercise/exercise_2.cpp
@@ -0,0 +1,17 @@
+//read fahrenheit input from user and write celsius output
+
+
+#include <iostream>
+//solve
+int main(){
+    double f;
+    double c;
+
+    
+
+    std :: cout << "fahrenheit input"; //state tax
+    std :: cin >> f;//input
+    c=(f-32)/1.8;
+    std :: cout << c;
+   return 0;
+}
